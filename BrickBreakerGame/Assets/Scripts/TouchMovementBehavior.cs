3fa911b3ee74605fedf6a9af6c796baa0192cce7
Amanda <10932525@uvu.edu>
2023-09-27 23:17:22 -0600

    Brick(Start of paddle script)

--- BrickBreakerGame/Assets/Scripts/TouchMovementBehavior.cs
+++ BrickBreakerGame/Assets/Scripts/TouchMovementBehavior.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchMovementBehavior : MonoBehaviour
 {
-    // Writing a line alongside 
-    void Start()
+    public Rigidbody Rigidbody { get; private set; }
+    private Vector2 startPosition; 
+    
+
+    private void Awake()
     {
+        this.Rigidbody = GetComponent<Rigidbody>();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleTouchInput()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0); // Get the first touch
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    // Store the initial touch position
+                    startPosition = touch.position;
+                    break;
+                case TouchPhase.Moved:
+                    // Calculate the difference between the initial and current touch position
+                    float deltaX = touch.position.x - startPosition.x;
+
+                    // Move the game object based on the calculated delta
+                    transform.Translate(new Vector3(deltaX * Time.deltaTime, 0, 0));
+                    break;
+            }
+        }
     }
-}
+
+    }
