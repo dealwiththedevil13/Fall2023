7644679548aac6e5cc33cbc3cd68dc57662354b8
Amanda <10932525@uvu.edu>
2023-10-03 02:00:07 -0600

    BB( Worked on the ball movement)

--- BrickBreakerGame/Assets/Scripts/BallMovementBehavior.cs
+++ BrickBreakerGame/Assets/Scripts/BallMovementBehavior.cs
@@ -2,14 +2,26 @@ using UnityEngine;
 
 public class BallMovementBehavior : MonoBehaviour
 {
-  public object Rigidbody { get; set; }
-  void Start()
+  public new Rigidbody rigidbody;
+  public float speed = 500f;
+
+  private void Awake()
   {
-    if (GetComponent(<Rigidbody > () == null)
-    {
-      Rigidbody rb = gameObject.AddComponent<Rigidbody>();
-    }
+    this.rigidbody = GetComponent<Rigidbody>();
   }
 
-  
+  private void Start()
+  {
+    Invoke(nameof(RandomTrajectory), 1f);
+    
+  }
+
+  private void RandomTrajectory()
+  {
+    Vector3 force = Vector2.zero;
+    force.x = Random.Range(-1f, 1f);
+    force.y = -1f;
+    
+    this.rigidbody.AddForce(force.normalized * this.speed);
+  }
 }
